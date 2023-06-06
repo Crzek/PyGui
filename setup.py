f8ffde128b9d3c196c5568ed18fe8f56c386bc56
CruzWin <javier1996.ec@gmail.com>
2023-06-06 20:03:07 +0200

    pakage /dir/..

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='pyguix',
+    version='1.0.0',
+    packages=['pyguix'],
+    install_requires=[
+        "PyQt6",
+        'configparser'
+    ],
+    author='Erick Cruz',
+    author_email='crzerick6@gmail.com',
+    description='generate fast graphical interfaces',
+    url='https://github.com/Crzek/pyguix',
+)
